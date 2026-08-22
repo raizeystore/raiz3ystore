8e2191e27a4b2a7cb9b19beee1c17060ae70da5c
raizeystore <raizeystore@gmail.com>
2026-08-22 20:20:31 +0200

    feat: activate Sentry browser monitoring with project DSN

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
-const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const PUBLIC_SENTRY_DSN = "https://901df7042fdba7a82728a99929c21355@o4511954068504576.ingest.us.sentry.io/4511955816087552";
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN ?? PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn,
